e1c491fbe4a36ce1744c811537839a0a6498af51
djumanov <diyorbekjumanov1120@gmail.com>
2022-11-09 14:57:36 +0500

    remaining files

---
+++ least_expensive.py
@@ -0,0 +1,10 @@
+# Find the least expensive expense
+def least_expensive(monthly_expenses: dict) -> str:
+    """
+    Find the least expensive expense
+    Args:
+        monthly_expenses: dictionary of monthly expenses
+    Returns:
+        least_expensive: least expensive expense
+    """
+    return
---
+++ most_expensive.py
@@ -0,0 +1,10 @@
+# Find the most expensive expense
+def most_expensive(monthly_expenses: dict) -> str:
+    """
+    Find the most expensive expense
+    Args:
+        monthly_expenses: dictionary of monthly expenses
+    Returns:
+        most_expensive: most expensive expense
+    """
+    return
